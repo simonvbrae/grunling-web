5f0a8dac44e8ef68e2af2c5a3ded122dd641df1d
Simon Van Braeckel <simonvanbraeckel@gmail.com>
2022-11-26 17:35:25 +0100

    Add one hamburger menu idea

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -9,10 +9,20 @@
         >
           <LogoComponent style="width: 16.5vw; min-width: 180px" />
         </v-col>
-        <v-col cols="1" class="menuItem"> About </v-col>
-        <v-col cols="1" class="menuItem"> Offer </v-col>
-        <v-col cols="1" class="menuItem"> Invest </v-col>
-        <v-col cols="1" class="menuItem"> Contact </v-col>
+        <!-- <v-col cols="1" class="menuItem" :v-if="!phone"> About </v-col>
+        <v-col cols="1" class="menuItem" :v-if="!phone"> Offer </v-col>
+        <v-col cols="1" class="menuItem" :v-if="!phone"> Invest </v-col>
+        <v-col cols="1" class="menuItem" :v-if="!phone"> Contact </v-col> -->
+        <label :v-if="phone">
+          <input type="checkbox" />
+          <span class="menu"> <span class="hamburger"></span> </span>
+          <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Offer</a></li>
+            <li><a href="#">Invest</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </label>
       </v-row>
     </v-container>
   </div>
@@ -25,6 +35,11 @@ import LogoComponent from "./LogoComponent.vue";
 export default Vue.extend({
   name: "MenuBar",
   components: { LogoComponent },
+  computed: {
+    phone() {
+      return true;
+    },
+  },
 });
 </script>
 <style scoped>
@@ -47,5 +62,103 @@ export default Vue.extend({
 
   min-width: 10%;
   margin-right: 2%;
+}
+
+label .menu {
+  position: absolute;
+  right: -100px;
+  top: -100px;
+  z-index: 100;
+  width: 200px;
+  height: 200px;
+  background: #fff;
+  border-radius: 50% 50% 50% 50%;
+  -webkit-transition: 0.5s ease-in-out;
+  transition: 0.5s ease-in-out;
+  box-shadow: 0 0 0 0 #fff, 0 0 0 0 #fff;
+  cursor: pointer;
+}
+
+label .hamburger {
+  position: absolute;
+  top: 135px;
+  left: 50px;
+  width: 30px;
+  height: 2px;
+  background: #69d2e7;
+  display: block;
+  -webkit-transform-origin: center;
+  transform-origin: center;
+  -webkit-transition: 0.5s ease-in-out;
+  transition: 0.5s ease-in-out;
+}
+
+label .hamburger:after,
+label .hamburger:before {
+  -webkit-transition: 0.5s ease-in-out;
+  transition: 0.5s ease-in-out;
+  content: "";
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: #69d2e7;
+}
+
+label .hamburger:before {
+  top: -10px;
+}
+
+label .hamburger:after {
+  bottom: -10px;
+}
+
+label input {
+  display: none;
+}
+
+label input:checked + .menu {
+  box-shadow: 0 0 0 100vw #fff, 0 0 0 100vh #fff;
+  border-radius: 0;
+}
+
+label input:checked + .menu .hamburger {
+  -webkit-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
+label input:checked + .menu .hamburger:after {
+  -webkit-transform: rotate(90deg);
+  transform: rotate(90deg);
+  bottom: 0;
+}
+
+label input:checked + .menu .hamburger:before {
+  -webkit-transform: rotate(90deg);
+  transform: rotate(90deg);
+  top: 0;
+}
+
+label input:checked + .menu + ul {
+  opacity: 1;
+}
+
+label ul {
+  z-index: 200;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  opacity: 0;
+  -webkit-transition: 0.25s 0s ease-in-out;
+  transition: 0.25s 0s ease-in-out;
+}
+
+label a {
+  margin-bottom: 1em;
+  display: block;
+  color: #f38630;
+  text-decoration: none;
 }
 </style>
