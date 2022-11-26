4950218a53492c2391456d28b7b4757c3b574932
Simon Van Braeckel <simonvanbraeckel@gmail.com>
2022-11-26 14:40:38 +0100

    First version Offer page

---
+++ src/views/OfferView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="contentWrapper" style="padding-top: 3vh">
+    <img class="offerImage" src="@/assets/images/offer.png" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "OfferView",
+  components: {},
+  computed: {},
+  methods: {},
+});
+</script>
+
+<style scoped>
+.offerImage {
+  margin: auto;
+  width: 70%;
+  height: 70%;
+}
+</style>
