1c2f05b6eb4311df8c76834a281c821f646d6853
Simon Van Braeckel <simonvanbraeckel@gmail.com>
2022-11-29 11:37:06 +0100

    Add first version hamburger menu

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -9,10 +9,51 @@
         >
           <LogoComponent style="width: 16.5vw; min-width: 180px" />
         </v-col>
-        <v-col cols="1" class="menuItem"> About </v-col>
+        <!-- <v-col cols="1" class="menuItem"> About </v-col>
         <v-col cols="1" class="menuItem"> Offer </v-col>
         <v-col cols="1" class="menuItem"> Invest </v-col>
-        <v-col cols="1" class="menuItem"> Contact </v-col>
+        <v-col cols="1" class="menuItem"> Contact </v-col> -->
+        <input id="menu-toggle" type="checkbox" />
+        <label class="menu-button-container" for="menu-toggle">
+          <div class="menu-button"></div>
+        </label>
+        <ul class="menu sectionContent">
+          <li>
+            <router-link
+              style="text-decoration-line: none; color: white"
+              to="/about"
+              >About</router-link
+            >
+          </li>
+          <li>
+            <router-link
+              style="text-decoration-line: none; color: white"
+              to="/about2"
+              >About2</router-link
+            >
+          </li>
+          <li>
+            <router-link
+              style="text-decoration-line: none; color: white"
+              to="/offer"
+              >Offer</router-link
+            >
+          </li>
+          <li>
+            <router-link
+              style="text-decoration-line: none; color: white"
+              to="/invest"
+              >Invest</router-link
+            >
+          </li>
+          <li>
+            <router-link
+              style="text-decoration-line: none; color: white"
+              to="/contact"
+              >Contact</router-link
+            >
+          </li>
+        </ul>
       </v-row>
     </v-container>
   </div>
@@ -46,6 +87,139 @@ export default Vue.extend({
 </script>
 
 <style scoped>
+@import url(https://fonts.googleapis.com/css?family=Raleway);
+
+/* * {
+  font-family: "Raleway";
+  box-sizing: border-box;
+} */
+
+/* .top-nav {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #00baf0;
+  background: linear-gradient(to left, #f46b45, #eea849);
+  color: #fff;
+  height: 50px;
+  padding: 1em;
+} */
+
+.menu {
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.menu > li {
+  margin: 0 1rem;
+  overflow: hidden;
+}
+
+.menu-button-container {
+  margin: auto;
+  margin-right: 10%;
+  display: none;
+  height: 100%;
+  width: 30px;
+  cursor: pointer;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+#menu-toggle {
+  display: none;
+}
+
+.menu-button,
+.menu-button::before,
+.menu-button::after {
+  display: block;
+  background-color: #fff;
+  position: absolute;
+  height: 4px;
+  width: 30px;
+  transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  border-radius: 2px;
+}
+
+.menu-button::before {
+  content: "";
+  margin-top: -8px;
+}
+
+.menu-button::after {
+  content: "";
+  margin-top: 8px;
+}
+
+#menu-toggle:checked + .menu-button-container .menu-button::before {
+  margin-top: 0px;
+  transform: rotate(405deg);
+}
+
+#menu-toggle:checked + .menu-button-container .menu-button {
+  background: rgba(255, 255, 255, 0);
+}
+
+#menu-toggle:checked + .menu-button-container .menu-button::after {
+  margin-top: 0px;
+  transform: rotate(-405deg);
+}
+
+@media (max-width: 700px) {
+  .menuItem {
+    text-decoration: none;
+  }
+  .menu-button-container {
+    display: flex;
+  }
+  .menu {
+    position: absolute;
+    top: 0;
+    margin-top: 19vh;
+    left: 0;
+    flex-direction: column;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+  #menu-toggle ~ .menu li {
+    height: 0;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  }
+  #menu-toggle:checked ~ .menu li {
+    border: 1px solid #333;
+    height: 2.5em;
+    padding: 0.5em;
+    transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  }
+  .menu > li {
+    display: flex;
+    justify-content: center;
+    margin: 0;
+    padding: 0.5em 0;
+    width: 100%;
+    color: white;
+    background-color: #222;
+  }
+  .menu > li:not(:last-child) {
+    border-bottom: 1px solid #444;
+  }
+}
+/**HAAAAAAAAAAAAAAAaaaa */
+/**HAAAAAAAAAAAAAAAaaaa */
+/**HAAAAAAAAAAAAAAAaaaa */
+/**HAAAAAAAAAAAAAAAaaaa */
+/**HAAAAAAAAAAAAAAAaaaa */
+
 .headerBar {
   display: block;
   block-size: fit-content;
